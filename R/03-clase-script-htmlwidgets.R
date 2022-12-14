365fb22328a6dd06640ad76d2bcd84984310bebc
Joshua Kunst <jbkunst@gmail.com>
2022-12-14 20:07:58 -0300

    actualizando

--- R/03-clase-script-htmlwidgets.R
+++ R/03-clase-script-htmlwidgets.R
@@ -1,13 +1,18 @@
 # plotly ------------------------------------------------------------------
 library(ggplot2)
 library(plotly)
+library(dplyr)
 
 data(iris)
 
+glimpse(iris)
+
 p <- ggplot(iris, aes(Sepal.Length, Sepal.Width)) +
   geom_point() +
-  geom_smooth(method = "lm") + 
+  geom_smooth(method = "lm") +
   facet_wrap(vars(Species))
+
+p
 
 ggplotly(p)
 
@@ -17,6 +22,10 @@ library(highcharter)
 library(forecast)
 
 data("AirPassengers")
+
+plot(AirPassengers)
+
+hchart(AirPassengers)
 
 hchart(AirPassengers, name = "Serie temporal", color = "red")
 
@@ -29,30 +38,31 @@ hchart(modelo)
 library(DT)
 library(rvest)   # descargar datos de paginas web
 
-url <- "http://www.sismologia.cl/ultimos_sismos.html"
+url <- "https://www.sismologia.cl/sismicidad/catalogo/2022/07/20220721.html"
 
-datos <- read_html(url) %>% 
-  html_table() %>% 
-  dplyr::first()
+datos <- read_html(url) |>
+  html_table() |>
+  dplyr::nth(2) |>
+  janitor::clean_names() |>
+  tidyr::separate(
+    latitud_longitud,
+    into = c("latitud", "longitud"),
+    sep = " ", convert = TRUE
+  )
+
+datos
+
+glimpse(datos)
 
 datatable(datos)
 
 # leaflet -----------------------------------------------------------------
-library(leaflet)
-library(rvest)   # descargar datos de paginas web
-
-url <- "http://www.sismologia.cl/ultimos_sismos.html"
-
-datos <- read_html(url) %>% 
-  html_table() %>% 
-  dplyr::first()
-
 leaflet(datos) %>%
-  addTiles() %>%  
+  addTiles() %>%
   addMarkers(
-    lng = ~Longitud, 
-    lat = ~Latitud,
-    popup = ~as.character(Magnitud),
-    label = ~as.character(`Referencia Geográfica`)
+    lng = ~longitud,
+    lat = ~latitud,
+    popup = ~as.character(magnitud_2),
+    label = ~as.character(fecha_local_lugar )
   )
 
